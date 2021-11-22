a501d15f24dd7c62f608d5f82aabad5acc325c2e
Johan van der Heide <info@jield.nl>
2021-11-22 21:08:02 +0100

    Quick disable of the translator cache (does not work anymore with latest laminas cache

--- config/autoload/cache.global.php
+++ config/autoload/cache.global.php
@@ -1,24 +1,21 @@
 <?php
 
-$cache = [
-    'adapter' => [
-        'options' => [
-            'server'    => [
-                'host' => 'redis',
-                'port' => 6379,
-            ],
-            'database'  => 1,
-            'namespace' => 'pa-portal',
-        ],
-    ],
-];
+declare(strict_types=1)
 
 return [
     'application_config' => [
-        'cache_options' => $cache,
-    ],
-    'translator'         => [
-        'cache' => $cache,
+        'cache_options' => [
+            'adapter' => [
+                'options' => [
+                    'server'    => [
+                        'host' => 'redis',
+                        'port' => 6379,
+                    ],
+                    'database'  => 1,
+                    'namespace' => 'pa-portal',
+                ],
+            ],
+        ],
     ],
 ];
 
